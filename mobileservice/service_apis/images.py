669792903111ee38acd3a65e2eb3c6bb58a0ccd1
Akshay <akshay@nimbrisk.com>
2017-09-26 18:00:33 +0530

    imges service changes

--- mobileservice/service_apis/images.py
+++ mobileservice/service_apis/images.py
@@ -6,22 +6,22 @@ import cloudinary.uploader
 import cloudinary.api
 from __builtin__ import file
 
-from uni_db.mob_app.models import Customer
+from uni_db.mob_app.models import Shop_Images
 
 class Cust_Image(Resource):
 
     def post(self):
         print "Hello"
         file = request.files['image']
-#         data = request.get_json(force=True)
+        data = request.values.to_dict()
         print file
-#         print data
-        cloudinary.config(cloud_name="http-nimbrisk-com",
-                          api_key="854195629923224",
-                          api_secret="swPL_-5N9bzrMZWZofMi95b5wCM")
-
-        response = cloudinary.uploader.upload(request.FILES['image'])
-        print response
+        print data
+#         cloudinary.config(cloud_name="http-nimbrisk-com",
+#                           api_key="854195629923224",
+#                           api_secret="swPL_-5N9bzrMZWZofMi95b5wCM")
+# 
+#         response = cloudinary.uploader.upload(request.FILES['image'])
+#         print response
         return{
                     'responseCode': 200,
                     'Message': "Image Uploaded Successfully."
